c348b31bd08210b7b8931f5198c2aab1e876d412
ArgentumLunae <ArgentumLunae@gmail.com>
2025-06-10 23:43:27 +0200

    part one all but finished, make good main

--- ex/ex00/DivideAndRule.cpp
+++ ex/ex00/DivideAndRule.cpp
@@ -4,37 +4,28 @@
 
 #define FEE_PERCENTAGE 5
 
-struct Account
-{
-	private:
-		int id;
-		int value;
-
-		public:
-		Account() :
-		id(-1),
-		value(0)
-		{
-			
-		}
-		
-		const int &getID() const
-		{
-			return id;
-		}
-		
-		friend std::ostream& operator << (std::ostream& p_os, const Account& p_account)
-		{
-			p_os << "[" << p_account.id << "] - [" << p_account.value << "]";
-			return (p_os);
-		}
-
-		friend struct Bank;
-};
+#define ACC_NOTFOUND "Account not found in bank system."
 
 struct Bank
 {
 	private:
+		struct Account
+		{		
+			private:
+				int id;
+				int value;
+
+			public:
+				Account() :	id(-1), value(0) {}
+				const int &getID() const { return id;}
+				friend std::ostream& operator << (std::ostream& p_os, const Account& p_account)
+				{
+					p_os << "[" << p_account.id << "] - [" << p_account.value << "]";
+					return (p_os);
+				}
+				friend struct Bank;
+		};
+
 		int liquidity;
 		std::vector<Account *> clientAccounts;
 
@@ -57,10 +48,22 @@ struct Bank
 			}
 			if (id < 0)
 			{
-				std::cerr << "Account limit (" << INTMAX_MAX << ") reached." << std::endl << "Remove accounts before adding more." << std::endl;
+				std::cerr << "Account limit (" << __INT_MAX__ << ") reached." << std::endl << "Remove accounts before adding more." << std::endl;
 				return -1;
 			}
 			return id;
+		}
+
+		Account * findAccount(int &id)
+		{
+			for (std::vector<Account *>::iterator iter = clientAccounts.begin(); iter != clientAccounts.end(); iter++)
+			{
+				if ((*iter)->id == id)
+				{
+					return *iter;
+				}
+			}
+			return NULL;
 		}
 
 	public:
@@ -76,13 +79,13 @@ struct Bank
 				delete clientAccount;
 		}
 
-		Account	&createAccount()
+		const int&	createAccount()
 		{
 			Account	*newAccount = new Account();
 			newAccount->id = generateID();
 			newAccount->value = 0;
 			clientAccounts.push_back(newAccount);
-			return *newAccount;
+			return newAccount->id;
 		}
 
 		void	deleteAccount(int id)
@@ -98,25 +101,52 @@ struct Bank
 			}
 		}
 
-		void	deleteAccount(Account &rmAccount)
+		bool	depositToAccount(int id, int depositValue)
 		{
-			for (std::vector<Account *>::iterator iter = clientAccounts.begin(); iter != clientAccounts.end(); iter++)
+			Account *depositAccount = findAccount(id);
+			if (depositAccount)
 			{
-				std::cout << *iter << "\t" << &rmAccount << std::endl;
-				if (*iter == &rmAccount)
-				{
-					clientAccounts.erase(iter);
-					delete &rmAccount;
-					return ;
-				}
+				int transferFee = depositValue * FEE_PERCENTAGE / 100;
+				liquidity += transferFee;
+				depositAccount->value += (depositValue - transferFee);
+				return true;
 			}
+			std::cout << "Depostit failed: " << ACC_NOTFOUND << std::endl;
+			return false;
 		}
 
-		void	depositToAccount(Account &depositAccount, int depositValue)
+		bool	withdrawFromAccount(int id, int withdrawalValue)
 		{
-			int transferFee = depositValue * FEE_PERCENTAGE / 100;
-			liquidity += transferFee;
-			depositAccount.value += (depositValue - transferFee);
+			Account *withdrawalAccount = findAccount(id);
+			if (!withdrawalAccount)
+			{
+				std::cout << "Withdrawal failed: " << ACC_NOTFOUND << std::endl;
+				return false;
+			}
+			if (withdrawalValue > withdrawalAccount->value)
+			{
+				std::cout << "Withdrawal failed: Not enough funds in account." << std::endl;
+			}
+			withdrawalAccount->value -= withdrawalValue;
+			return true;
+		}
+
+		bool	loanToAccount(int id, int loanValue)
+		{
+			if (loanValue > liquidity)
+			{
+				std::cout << "Loan failed: not enough bank liquidity" << std::endl;
+				return false;
+			}
+			Account *loanAccount = findAccount(id);
+			if (loanAccount)
+			{
+				loanAccount->value += loanValue;
+				liquidity -= loanValue;
+				return true;
+			}
+			std::cout << "Loan failed: " << ACC_NOTFOUND << std::endl;
+			return false;
 		}
 
 		friend std::ostream& operator << (std::ostream& p_os, const Bank& p_bank)
@@ -131,15 +161,19 @@ struct Bank
 
 int main()
 {
-	Account accountA = Account();
+	//Account accountA = Account();
 	// accountA.id = 0;
 	// accountA.value = 100;
 	
-	Account accountB = Account();
+	//Account accountB = Account();
 	// accountB.id = 1;
 	// accountB.value = 100;
 	
 	Bank bank = Bank();
+	int accountA_ID = bank.createAccount();
+	bank.depositToAccount(accountA_ID, 100);
+	bank.loanToAccount(accountA_ID, 5);
+	bank.withdrawFromAccount(accountA_ID, 100);
 	// bank.liquidity = 999;
 	// bank.clientAccounts.push_back(&accountA);
 	// bank.clientAccounts.push_back(&accountB);
@@ -147,21 +181,20 @@ int main()
 	// bank.liquidity -= 200;
 	// accountA.value += 400;
 
-	Account& accountC = bank.createAccount();
-	std::cout << &accountC << std::endl;
-	bank.depositToAccount(accountC, 500);
+	//Account& accountC = bank.createAccount();
+	//std::cout << &accountC << std::endl;
 
 	std::cout << "Account : " << std::endl;
-	std::cout << accountA << std::endl;
-	std::cout << accountB << std::endl;
-	std::cout << accountC << std::endl;
+	// std::cout << accountA << std::endl;
+	// std::cout << accountB << std::endl;
+	// std::cout << accountC << std::endl;
 
 	std::cout << " ----- " << std::endl;
 
 	std::cout << "Bank : " << std::endl;
 	std::cout << bank << std::endl;
 
-	bank.deleteAccount(accountC);
+	bank.deleteAccount(accountA_ID);
 
 	return (0);
 }
--- ex/ex00/getSources.sh
+++
@@ -1,2 +0,0 @@
-echo -n "SRC_FILES := " > sources.mk
-find . -type f | grep ".cpp" | sed -e 's/$/ \\/' >> sources.mk
